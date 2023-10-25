3b19fe5cf9542dcbefc7f2bcee8a32410c158210
Mohammad varrel bramasta <mohammad.varrel@ui.ac.id>
2023-10-25 10:54:06 +0700

    Revisi Query 1

--- Database/QuerySQL.sql
+++ Database/QuerySQL.sql
@@ -1,5 +1,26 @@
 -- Jenis ENUM untuk peran (Role) pada tabel account 
-CREATE TYPE user_role AS ENUM ('FisherMan', 'Traders');
+CREATE TYPE user_role AS ENUM (
+    'FisherMan', 
+    'Traders'
+);
+
+-- Jenis ENUM untuk delivery status pada table orders
+CREATE TYPE delivery_status_enum AS ENUM (
+    'Pending',
+    'Processing',
+    'Shipped',
+    'Delivered',
+    'Canceled'
+);
+
+-- Jenis ENUM untuk Details pada table Payment
+CREATE TYPE payment_details_enum AS ENUM (
+    'Credit Card',
+    'Debit Card',
+    'PayPal',
+    'Bank Transfer',
+    'Cash'
+);
 
 -- CREATE TABLE Account (
 --     AccoundID BIGSERIAL PRIMARY KEY NOT NULL,
@@ -13,7 +34,7 @@ CREATE TYPE user_role AS ENUM ('FisherMan', 'Traders');
 
 -- Tabel "Account_Traders"
 CREATE TABLE Account_Traders (
-    TradersID BIGSERIAL PRIMARY KEY NOT NULL,
+    TradersID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
     Name VARCHAR(255),
     Email VARCHAR(255) UNIQUE,
     Password VARCHAR(255) UNIQUE,
@@ -24,59 +45,51 @@ CREATE TABLE Account_Traders (
 
 -- Tabel "Account_Fisherman"
 CREATE TABLE Account_Fisherman (
-    FishermanID BIGSERIAL PRIMARY KEY NOT NULL,
+    FishermanID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
     Name VARCHAR(255),
     Email VARCHAR(255) UNIQUE,
+    Password VARCHAR(255) UNIQUE,
     Address VARCHAR(255),
-    Phone INT
+    Phone INT UNIQUE,
     Role user_role
-);
-
-
--- Tabel "Shopping_Cart"
-CREATE TABLE Shopping_Cart (
-    ShoppingCartID BIGSERIAL PRIMARY KEY NOT NULL,
-    Created DATE,
-    FOREIGN KEY (TradersID) REFERENCES Account_Traders (TradersID)
-    -- FOREIGN KEY (AccoundID) REFERENCES Account (AccoundID)
-);
-
--- Tabel "Reviews"
-CREATE TABLE Reviews (
-    ReviewID BIGSERIAL PRIMARY KEY NOT NULL,
-    FOREIGN KEY (TradersID) REFERENCES Account_Traders (TradersID),
-    -- FOREIGN KEY (AccoundID) REFERENCES Account (AccoundID),
-    ReviewContent VARCHAR(255),
-    Rating INT,
-    FOREIGN KEY (ProductID) REFERENCES Product (ProductID)
 );
 
 -- Tabel "Product"
 CREATE TABLE Product (
-    ProductID BIGSERIAL PRIMARY KEY NOT NULL,
+    ProductID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
     ProductName VARCHAR(255),
     ProductCost INT,
     FOREIGN KEY (FishermanID) REFERENCES Account_Fisherman (FishermanID),
     FOREIGN KEY (TradersID) REFERENCES Account_Traders (TradersID),
     -- FOREIGN KEY (AccoundID) REFERENCES Account (AccoundID),
     PostedDate DATE,
-    ProductIMG byte[], --bingung
-    Description TEXT,
+    ProductIMG bytea, --labil
+    Description VARCHAR(255),
     CatchDate DATE
 );
 
--- Tabel "Payment"
-CREATE TABLE Payment (
-    PaymentID BIGSERIAL PRIMARY KEY NOT NULL,
-    FOREIGN KEY (OrderID) REFERENCES Orders (OrderID),
-    Paid BOOLEAN,
-    Total DECIMAL,
-    Details TEXT
+-- Tabel "Shopping_Cart"
+CREATE TABLE Shopping_Cart (
+    ShoppingCartID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
+    Created DATE,
+    FOREIGN KEY (TradersID) REFERENCES Account_Traders (TradersID),
+    -- FOREIGN KEY (AccoundID) REFERENCES Account (AccoundID),
+    FOREIGN KEY (ProductID) REFERENCES Product (ProductID)
+);
+
+-- Tabel "Reviews"
+CREATE TABLE Reviews (
+    ReviewID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
+    FOREIGN KEY (TradersID) REFERENCES Account_Traders (TradersID),
+    -- FOREIGN KEY (AccoundID) REFERENCES Account (AccoundID),
+    FOREIGN KEY (ProductID) REFERENCES Product (ProductID),
+    ReviewContent VARCHAR(255),
+    Rating INT
 );
 
 -- Tabel "Orders"
 CREATE TABLE Orders (
-    OrderID BIGSERIAL PRIMARY KEY NOT NULL,
+    OrderID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
     FOREIGN KEY (TradersID) REFERENCES Account_Traders (TradersID),
     FOREIGN KEY (FishermanID) REFERENCES Account_Fisherman (FishermanID),
     -- FOREIGN KEY (AccoundID) REFERENCES Account (AccoundID),
@@ -84,5 +97,16 @@ CREATE TABLE Orders (
     TotalAmount DECIMAL,
     OrderDate DATE,
     DeliveryDate DATE,
-    DeliveryStatus TEXT
+    DeliveryStatus delivery_status_enum
 );
+
+-- Tabel "Payment"
+CREATE TABLE Payment (
+    PaymentID BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
+    FOREIGN KEY (OrderID) REFERENCES Orders (OrderID),
+    Paid BOOLEAN,
+    Total DECIMAL,
+    Details payment_details_enum
+);
+
+

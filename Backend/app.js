7fafe577cd23460859adf2be54d72d208c070e72
Mohammad varrel bramasta <mohammad.varrel@ui.ac.id>
2023-10-26 10:09:28 +0700

    Update app.js 1

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,17 @@
 import express from "express";
 
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use( "/api", (req, res, next) => {
-    res.send("Hello World");
+app.get('/', (req, res) => {
+    if(req.user){
+        res.send(req.user);and
+    }else{
+        res.send('Not logged in');
+    }
+    //res.json({ message: 'Welcome to the mm backend' }); 
 });
 
-app.listen(5000);
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+});

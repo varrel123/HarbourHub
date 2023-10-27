d3f881ba86857ff3a4e15e62b1d8a265a58150ec
Mohammad varrel bramasta <mohammad.varrel@ui.ac.id>
2023-10-27 22:03:18 +0700

    Update Backend Checkallaccount & login

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,9 @@ const session = require('express-session');
 const cors = require('cors');
 const passport = require('passport');
 const MemoryStore = require('session-memory-store')(session); // Impor MemoryStore
-const HBService = require('./src/Services/hb.services.js');
-
 const app = express();
+const HBService = require('./src/Services/hb.services');
+
 
 // Konfigurasi store
 const store = new MemoryStore();
@@ -41,6 +41,9 @@ app.get('/', (req, res) => {
     //res.json({ message: 'Welcome to the mm backend' }); 
 });
 
+                
+              //Controller//
+
 app.get('/check_all_account', (req, res) => {
     HBService.checkAllAccounts((err, accounts) => {
       if (err) {
@@ -50,8 +53,38 @@ app.get('/check_all_account', (req, res) => {
       }
     });
   });
-  
 
+  app.post('/login_fisherman', (req, res) => {
+    const { Email, Password } = req.body;
+  
+    HBService.loginFisherman({ Email, Password }, (err, fishermanAccount) => {
+      if (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else if (fishermanAccount) {
+        res.json({ message: 'Login berhasil', fishermanAccount });
+      } else {
+        res.json({ message: 'Login gagal, akun tidak ditemukan atau password tidak sesuai.' });
+      }
+    });
+  });
+  
+  app.post('/login_traders', (req, res) => {
+    const { Email, Password } = req.body;
+  
+    HBService.loginTraders({ Email, Password }, (err, tradersAccount) => {
+      if (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else if (tradersAccount) {
+        res.json({ message: 'Login berhasil', tradersAccount });
+      } else {
+        res.json({ message: 'Login gagal, akun tidak ditemukan atau password tidak sesuai.' });
+      }
+    });
+  });
+
+
+  
+////////////////
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 });
--- Backend/src/Services/hb.services.js
+++ Backend/src/Services/hb.services.js
@@ -1,8 +1,10 @@
 const db = require('../Config/dbconfig.js');
+const helper = require('../Utils/Helper.js');
 
 //Check Semua account yang ada
 function checkAllAccounts(callback) {
-  db.query('SELECT * FROM Account', (err, accountResults) => {
+  const query = `SELECT * FROM Account`;
+  db.query(query, (err, accountResults) => {
     if (err) {
       console.log(err);
       callback(err, null);
@@ -12,12 +14,51 @@ function checkAllAccounts(callback) {
   });
 }
 
-//Login
+//Login Account fisherMan
+function loginFisherman(mm, callback) {
+  const { Email, Password } = mm;
+  const query = `SELECT * FROM Account WHERE Email = '${Email}' AND Password = '${Password}' AND Role = 'FisherMan'`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      callback(err, null);
+    } else {
+      if (result.rows.length === 1) {
+        const fishermanAccount = result.rows[0];
+        callback(null, fishermanAccount);
+      } else {
+        callback(null, null);
+      }
+    }
+  });
+}
+
+//Login Account Traders
+function loginTraders(mm, callback) {
+  const { Email, Password } = mm;
+  const query = `SELECT * FROM Account WHERE Email = '${Email}' AND Password = '${Password}' AND Role = 'Traders'`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      callback(err, null);
+    } else {
+      if (result.rows.length === 1) {
+        const tradersAccount = result.rows[0];
+        callback(null, tradersAccount);
+      } else {
+        callback(null, null);
+      }
+    }
+  });
+}
+
 //Register
 //Delete
 //Edit
 
 
 module.exports = {
-  checkAllAccounts
+  checkAllAccounts,
+  loginFisherman,
+  loginTraders
 };

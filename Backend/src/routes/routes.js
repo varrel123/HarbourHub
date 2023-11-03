58974fb9e1d917d21d0b171890711b4ca4efbdfd
Mohammad varrel bramasta <mohammad.varrel@ui.ac.id>
2023-11-03 16:36:49 +0700

    Mengganti domain showuser

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -8,20 +8,15 @@ router.post('/loginTraders', HBController.loginTraders);
 
 router.post('/register', HBController.register);
 
-router.get('/showaccount', HBController.showUser);
+router.get('/showuser', HBController.showUser);
 
 router.delete('/deleteaccount', HBController.deleteUser);
 
 router.put("/updateaccount", HBController.UpdateAccount);
 
-<<<<<<< Updated upstream
 router.post("/addproduct", HBController.AddProduct);
 
 router.get("/showproduct", HBController.ShowProduct);
-=======
 // router.post('/addProductToCart', HBController.addProductToCart);
-
-
->>>>>>> Stashed changes
 
 module.exports = router;
